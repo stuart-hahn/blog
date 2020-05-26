1245d5df8bac79f2c679ba31a8fde3d5c10066c0
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-05-26 15:36:20 -0400

    remove fetchUser from UserHeader to prevent overfetching. Handled by fetchPostsAndUsers() action creator now

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
-// import _ from "lodash";
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log("about to fetch users");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log("fetched users");
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
